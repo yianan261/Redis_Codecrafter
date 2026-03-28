3f3e0a25ec61ad3e031f246e197c653163322de5
yianan261 <yian.chen0131@gmail.com>
2026-03-27 23:52:11 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <poll.h> 
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -43,35 +44,79 @@ int main(int argc, char **argv) {
     return 1;
   }
   
-  struct sockaddr_in client_addr;
-  int client_addr_len = sizeof(client_addr);
-  std::cout << "Waiting for a client to connect...\n";
+  // initialize event loop watchlist
+  std::vector<struct pollfd> fds;
 
+  // add server sockets to watchlist
+
+  struct pollfd server_pollfd;
+  server_pollfd.fd = server_fd;
+  server_pollfd.events = POLLIN; // watch for incoming data/connections
+  fds.push_back(server_pollfd);
+  
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  // Uncomment the code below to pass the first stage
   
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
-
   // use buffer and recv() in while loop (one client can send multiple PINGs
   // client sends multiple commands over one connection, 
   //they don't necessarily arrive one by one; they arrive as a continuous "stream" of bytes.
-  char buffer[1024];
   while(true){
-    ssize_t bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
-    if(bytes_read <= 0) break;
-    // const char pointer (stored in read-only data, stored in the binary that runs before main() for efficiency)
-    const char *response = "+PONG\r\n";
-    // Passing 0 tells the operating system to use the default behavior for sending data.
-    // For most basic network applications, the default is enough
-    send(client_fd, response, strlen(response), 0);
 
+    // wait for IS to tell a socket is ready
+    int ready_count = poll(fds.data(), fds.size(), -1);
+    
+    if (ready_count < 0){
+      std::cerr << "Poll failed\n";
+      break;
+    }
+
+    // loop through watched sockets to see which ones woke us up
+    for (size_t i = 0; i < fds.size(); i++){
+      // if revents is 0, nothing happened in this socket
+      if(fds[i].revents == 0) continue;
+
+      // check if POLLIN, using the bit manipulation
+      if(fds[i].revents & POLLIN){
+
+        if(fds[i].fd == server_fd){
+          // case 1: server socket is read, new client trying to connect
+          struct sockaddr_in client_addr;
+          int client_addr_len = sizeof(client_addr);
+          int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+          if(client_fd >= 0){
+            // add new client to our watchlist
+            struct pollfd client_pollfd;
+            client_pollfd.fd = client_fd;
+            client_pollfd.events = POLLIN;
+            fds.push_back(client_pollfd);
+          }
+          
+        }else{
+          // case 2: client socket is ready (or there's an error)
+          char buffer[1024];
+          int bytes_recevied = recv(fds[i].fd, buffer,sizeof(buffer),0);
+
+          // 0 means disconnect, negative means error 
+          if (bytes_recevied <= 0){ 
+            close(fds[i].fd);
+
+            // swap and pop for O(1) removal from vector
+            fds[i] = fds.back();
+            fds.pop_back();
+            // Decrement 'i' because we just swapped a new element 
+            // into the current index, and we need to check it.
+            i--;
+          }else{
+            // received data, we only need to fire PONG here
+            const char *response = "+PONG\r\n";
+            send(fds[i].fd, response, strlen(response),0);
+          }
+        }
+      }
+    }
   }
 
-  
-  close(client_fd);
   close(server_fd);
   
 
